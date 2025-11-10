f77af238a9f92a8e4f03304cfddc3d91c02fd89e
Vincent Handoyo <vincenthan28@gmail.com>
2025-11-11 00:14:34 +0800

    init prismaORM

---
+++ prisma.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "db/schema.prisma",
+  migrations: {
+    path: "db/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
