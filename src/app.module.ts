1e3fc6adc45789025ca915d9d9e0c93ad12d6260
Vincent Handoyo <vincenthan28@gmail.com>
2025-11-12 21:32:05 +0800

    feat: globalize prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { DataModule } from './data/data.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule, DataModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
